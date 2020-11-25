f669e56d61bf351a4f605037f2199363aba2c6eb
Kiki Dawson <kikiadawson@gmail.com>
2020-11-25 15:06:36 +0000

    added working method

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,7 +1,22 @@
 require 'docking_station'
+require 'bike'
 
 describe DockingStation do
   it 'responds to release_bike' do
     expect(subject).to respond_to :release_bike
   end
+
+  describe '#release_bike' do
+    docking_station = DockingStation.new
+    it 'bike is released' do
+      expect(docking_station.release_bike).to be_a Bike
+    end
+
+    it 'is the bike working?' do
+      bike = Bike.new
+      expect(bike.working?).to eq true
+    end
+
+  end
+
 end
