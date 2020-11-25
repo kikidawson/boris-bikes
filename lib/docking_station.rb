0b4687e62a1561797d3ef85e19dc8c561d425136
calavell <calavell@tcd.ie>
2020-11-25 17:17:05 +0000

    Added instance variable @bike

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,7 +1,13 @@
 class DockingStation
+attr_reader :bike
+
 
   def release_bike
-    bike = Bike.new
+    Bike.new
+  end
+
+  def dock(bike)
+    @bike = bike
   end
 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,12 +2,25 @@ require 'docking_station'
 require 'bike'
 
 describe DockingStation do
+  let(:docking_station) {DockingStation.new}
   it 'responds to release_bike' do
     expect(subject).to respond_to :release_bike
   end
 
+  it 'responds to dock_bike' do
+    bike = Bike.new
+    expect(subject.dock(bike)).to eq bike
+  end
+
+  it 'returns docked bikes' do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.bike).to eq bike
+  end
+
+  it { is_expected.to respond_to(:bike) }
+
   describe '#release_bike' do
-    docking_station = DockingStation.new
     it 'bike is released' do
       expect(docking_station.release_bike).to be_a Bike
     end
@@ -16,7 +29,6 @@ describe DockingStation do
       bike = Bike.new
       expect(bike.working?).to eq true
     end
-
   end
 
 end
